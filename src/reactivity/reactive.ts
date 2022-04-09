17640ed6d9528b180f330dbdc21f772617da64e0
linguihuang <1134948440@qq.com>
2022-04-09 19:06:51 +0800

    readonly

--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,17 +1,13 @@
-import { track, trigger } from "./effect";
+import { mutableHandlers, readonlyHandlers } from "./baseHandler";
 
-export const reactive = (raw) => {
-  return new Proxy(raw, {
-    get(target, key) {
-      const res = Reflect.get(target, key);
+export function reactive(raw) {
+  return createReactiveObject(raw, mutableHandlers);
+}
 
-      track(target, key);
-      return res;
-    },
-    set(target, key, value) {
-      const res = Reflect.set(target, key, value);
-      trigger(target, key);
-      return res;
-    },
-  });
-};
+export function readonly(raw) {
+  return createReactiveObject(raw, readonlyHandlers);
+}
+
+function createReactiveObject(target, baseHandles) {
+  return new Proxy(target, baseHandles);
+}
