8d20b0549d80e6bf7bb7d361cc7d132f01f8ffc0
linguihuang <1134948440@qq.com>
2022-04-12 23:16:46 +0800

    完善 stop

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 - [x] effect 收集依赖与触发
 - [x] scheduler
-- [x] stop 待完善
+- [x] stop
 - [x] readonly
 - [x] isReactive
 - [x] isReadonly
--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,6 +1,7 @@
 import { extend } from "../shared";
 
 let activeEffect;
+let shouldTrack = false;
 
 class ReactiveEffect {
   private _fn: any;
@@ -13,8 +14,18 @@ class ReactiveEffect {
     this.scheduler = scheduler;
   }
   run() {
+    //只有调用了stop的方法active才会为false，所以当active为false代表已经调用过了stop，此时直接调用回调函数并返回结果
+    if (!this.active) {
+      return this._fn();
+    }
+
+    shouldTrack = true;
     activeEffect = this;
-    return this._fn();
+
+    const res = this._fn();
+
+    shouldTrack = false;
+    return res;
   }
   stop() {
     if (this.active) {
@@ -39,6 +50,7 @@ function cleanupEffect(effect) {
 
 const targetMap = new Map();
 export const track = (target, key) => {
+  if (!isTracking()) return;
   let depsMap = targetMap.get(target);
   if (!depsMap) {
     depsMap = new Map();
@@ -49,11 +61,16 @@ export const track = (target, key) => {
     dep = new Set();
     depsMap.set(key, dep);
   }
-  if (!activeEffect) return;
+
+  if (dep.has(activeEffect)) return;
 
   dep.add(activeEffect);
   activeEffect.deps.push(dep);
 };
+
+function isTracking() {
+  return shouldTrack && activeEffect !== undefined;
+}
 
 export const trigger = (target, key) => {
   let depsMap = targetMap.get(target);
